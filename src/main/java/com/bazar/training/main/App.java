6518d05f38b2d574cc776767a95e1db3d21ae5ff
fokafr@yahoo.fr <fokafr@yahoo.fr>
2019-06-07 17:32:26 +0200

    test run

--- src/main/java/com/bazar/training/main/App.java
+++ src/main/java/com/bazar/training/main/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args ) throws Exception
     {
         //System.out.println( "Hello World!" );
-    	//FreeMarkerMainTest.excecute();
-    	TestAppQuicktart.execute();
+    	FreeMarkerMainTest.excecute();
+    	//TestAppQuicktart.execute();
     }
 }
---
+++ src/main/java/com/bazar/training/others/DumpTest.java
@@ -0,0 +1,14 @@
+package com.bazar.training.others;
+
+public class DumpTest {
+
+	public static void main(String[] args) {
+		int i= 0;
+		while(true) {
+			 i++;
+			System.out.println(i);
+		}
+
+	}
+
+}
